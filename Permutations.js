ea432e5784ffc5bd6aa5cf3b3db268388e8d1705
ruanyuyuan <765426599@qq.com>
2020-06-16 23:33:52 +0800

    permutation remove same

--- Permutations.js
+++ Permutations.js
@@ -1,12 +1,22 @@
 
 const swap = require('./swap')
 
+function swapAccepted(a, start, end) {
+  for (let i = start; i < end; i++) {
+    if (a[i] === a[end]) {
+      return false
+    }
+  }
+  return true
+}
+
 function auxPermutation(a, i, len, res) {
   if (i === len) {
     res.push([...a])
   } else {
     // 固定第i个元素，后续继续交换
     for (let j = i; j < a.length; j++) {
+      if (!swapAccepted(a, i, j)) continue
       swap(a, i, j)
       auxPermutation(a, i + 1, len, res)
       swap(a, i, j)
@@ -23,4 +33,4 @@ function permutation(a) {
 }
 
 
-console.log(permutation([1, 2, 3]))
+console.log(permutation([1, 2, 2, 3]))
